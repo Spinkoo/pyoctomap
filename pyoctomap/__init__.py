e841ed82de086ffe0d19b739eef14f0d12698f8a
Spinkoo <lespinkoo@gmail.com>
2025-09-26 16:35:21 +0200

    fixing the os version

--- pyoctomap/__init__.py
+++ pyoctomap/__init__.py
@@ -11,7 +11,7 @@ import platform
 from pathlib import Path
 
 # Version information
-__version__ = "0.1.14pre"
+__version__ = "0.1.15pre"
 __author__ = "Spinkoo"
 __email__ = "lespinkoo@gmail.com"
 
