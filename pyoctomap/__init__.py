ec2d01622bf222f17cc394775683bde679a94380
Spinkoo <lespinkoo@gmail.com>
2025-09-26 20:17:17 +0200

    Update Docker build workflow and version for pyoctomap
    - Add a step to combine wheels and source distributions into a single directory for easier upload.
    - Change the package directory for publishing to PyPI from './wheels' to './packages'.
    - Increment version number from 0.1.23pre to 0.1.24pre in the pyoctomap module.

--- pyoctomap/__init__.py
+++ pyoctomap/__init__.py
@@ -11,7 +11,7 @@ import platform
 from pathlib import Path
 
 # Version information
-__version__ = "0.1.23pre"
+__version__ = "0.1.24pre"
 __author__ = "Spinkoo"
 __email__ = "lespinkoo@gmail.com"
 
