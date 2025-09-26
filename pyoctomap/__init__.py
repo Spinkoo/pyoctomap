94d01425c8aac43da67162b50b1fcf7fd9b1c7ea
Spinkoo <lespinkoo@gmail.com>
2025-09-26 12:36:28 +0200

    working on manylinux versions

--- pyoctomap/__init__.py
+++ pyoctomap/__init__.py
@@ -11,7 +11,7 @@ import platform
 from pathlib import Path
 
 # Version information
-__version__ = "0.1.12pre"
+__version__ = "0.1.13pre"
 __author__ = "Spinkoo"
 __email__ = "lespinkoo@gmail.com"
 
